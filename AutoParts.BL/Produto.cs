6cf05fdbc9a055bbfd8c9484d7a52adfa8d702d2
Luiz Ricardo Abaque Coelho <luizracoelho@gmail.com>
2016-05-03 21:16:15 -0300

    Criação da Camada de Domínio

---
+++ AutoParts.BL/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoParts.BL
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
---
+++ AutoParts.BL/Servico.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoParts.BL
+{
+    public class Servico
+    {
+        public int ServicoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
